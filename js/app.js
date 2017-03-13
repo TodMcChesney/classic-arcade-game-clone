c9052c89ab9d0864435fb77aea1655cafbe2274d
TodMcChesney <codescribe.net@gmail.com>
2017-03-13 09:09:31 +0100

    feat: Add Player class outline
    Copied allEnemies class structure to create a basic outline for the
    Player class.

--- js/app.js
+++ js/app.js
@@ -24,7 +24,31 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // Variables applied to each of our instances go here,
 
+    // The image/sprite for our player, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+};
+
+// Update the player's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Player.handleInput() method.
+Player.prototype.handleInput = function() {
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
