53a04e6e0d6d591bb5b3cb479e4ff903ffddbe83
TodMcChesney <codescribe.net@gmail.com>
2017-03-13 17:40:56 +0100

    refactor: Add parameters to Enemy class

--- js/app.js
+++ js/app.js
@@ -1,25 +1,26 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+// Enemy class
+var Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // Multiply movement by the dt parameter
+    this.x = this.x + speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+// Now instantiate your enemy objects.
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [];
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -50,10 +51,7 @@ Player.prototype.handleInput = function() {
 
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-var allEnemies = [];
-
+// Now instantiate your player objects.
 // Place the player object in a variable called player
 
 
